095fe7f50e8b9d09d64ef17e0671a3d66205bf57
kamiarnold73 <kamilla_arnold24@unikino.edu.mx>
2024-10-15 08:01:27 -0700

    Agregar archivos de proyecto.

---
+++ DiasSemana/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace DiasSemana
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dame un número");
+            string dato = Console.ReadLine();
+
+            int dia = 0;
+            bool sePudoConvertir = false;
+            sePudoConvertir = int.TryParse(dato, out dia);
+            if (sePudoConvertir && dia >=1 && dia <=7) 
+            {
+                Console.WriteLine(sePudoConvertir + " " + dia);
+            }
+
+            
+        }
+    }
+}
