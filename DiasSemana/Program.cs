5e80e11edbc3761bd07e8a50bb462d2870eec978
kamiarnold73 <kamilla_arnold24@unikino.edu.mx>
2024-12-13 17:05:37 -0700

    completado

--- DiasSemana/Program.cs
+++ DiasSemana/Program.cs
@@ -10,12 +10,41 @@
             int dia = 0;
             bool sePudoConvertir = false;
             sePudoConvertir = int.TryParse(dato, out dia);
-            if (sePudoConvertir && dia >=1 && dia <=7) 
+            if (sePudoConvertir)
             {
-                Console.WriteLine(sePudoConvertir + " " + dia);
+                switch (dia)
+                {
+                    case 1:
+                        Console.WriteLine("Lunes");
+                        break;
+                    case 2:
+                        Console.WriteLine("Martes");
+                        break;
+                    case 3:
+                        Console.WriteLine("Miercoles");
+                        break;
+                    case 4:
+                        Console.WriteLine("Jueves");
+                        break;
+                    case 5:
+                        Console.WriteLine("Viernes");
+                        break;
+                    case 6:
+                        Console.WriteLine("Sabado");
+                        break;
+                    case 7:
+                        Console.WriteLine("Domingo");
+                        break;
+                    default:
+                        Console.WriteLine("$Dato incorrecto (switch): {dato}");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine($"El dato introducido es incorrecto: {dato}");
             }
 
-            
         }
     }
 }
